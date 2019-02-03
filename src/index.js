113a07ae4e6c0eeeead6882afbce8e1c06f21576
Shehab Nagy <shehab.nagy1998@gmail.com>
2019-02-03 19:27:54 +0200

    Configured store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { BrowserRouter, Route } from 'react-router-dom'
+import rootReducer from './store/rootReducer';
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <Route path="/" component={App} />
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/store/rootReducer.js
@@ -0,0 +1,24 @@
+const rndColors = ['#c66683', '#a677a6', '#6ccb99', '#b2a87f', '#ffb700', '#9695d3', '#ffa269', '#92b7bc', '#7a98ac',
+    '#ff6c11', '#e0a9bf', '#d5baa8'];
+
+const generateArray = (nums, min, max, colors = rndColors) => {
+    let id = 1;
+    return new Array(nums).fill(0).map(() => {
+        return {
+            id: id++,
+            color: colors[Math.floor(Math.random() * (colors.length - 1))],
+            height: Math.floor((Math.random() * (max - min)) + min),
+            width: "200px"
+        }
+    })
+}
+
+const initState = {
+    cards: generateArray(30, 100, 300)
+}
+
+const rootReducer = (state = initState, action) => {
+    return state
+}
+
+export default rootReducer
